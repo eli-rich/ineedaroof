2c553d99644d4a5a3276dbde0d2d5a833acad8ba
Eli Richardson <elijahrich02@icloud.com>
2023-12-04 21:36:23 -0600

    flip about me text

--- src/About.tsx
+++ src/About.tsx
@@ -4,13 +4,6 @@ const About: Component = () => {
   return (
     <>
       <section class='mx-auto mb-10 flex max-h-min w-4/5 max-w-lg flex-col justify-center gap-2 sm:flex-row'>
-        <img
-          src='/family.webp'
-          alt='placeholder'
-          class='mx-auto w-2/3 rounded-md border-2 border-blue-950 sm:w-1/2'
-          width={600}
-          height={800}
-        />
         <div class='text-center text-white sm:text-left'>
           <h2 class='text-xl'>About Me</h2>
           <p class=''>
@@ -19,6 +12,13 @@ const About: Component = () => {
             Quisque sit.
           </p>
         </div>
+        <img
+          src='/family.webp'
+          alt='placeholder'
+          class='mx-auto w-2/3 rounded-md border-2 border-blue-950 sm:w-1/2'
+          width={600}
+          height={800}
+        />
       </section>
     </>
   );
