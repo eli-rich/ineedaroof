2149ad7938deebc86b0337deafe29ebd50daae38
Eli Richardson <elijahrich02@icloud.com>
2023-12-05 18:30:58 -0600

    add about and remove bbh link from header

--- src/About.tsx
+++ src/About.tsx
@@ -4,18 +4,22 @@ const About: Component = () => {
   return (
     <>
       <section class='mx-auto mb-10 flex max-h-min w-4/5 max-w-lg flex-col justify-center gap-2 sm:flex-row'>
-        <div class='text-center text-white sm:text-left'>
-          <h2 class='text-xl'>About Me</h2>
+        <div class='order-1 text-center text-white sm:order-2 sm:text-left'>
+          <h2 class='text-xl'>About Us</h2>
           <p class=''>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tortor felis, dapibus non
-            turpis vel, bibendum bibendum lorem. Ut id massa et ex volutpat congue at eu leo.
-            Quisque sit.
+            We are a custom home builder who can make your vision a reality. Whether you want a cozy
+            starter home or a lavish dream home, we can handle any project, big or small. We also do
+            remodeling, addition, and roofing.
+            <br /> <br />
+            We care about every home and project we work on, and we deliver quality work with pride.
+            <br />
+            <br /> We are licensed, insured, and Fortified Home certified.
           </p>
         </div>
         <img
           src='/family.webp'
           alt='placeholder'
-          class='mx-auto w-2/3 rounded-md border-2 border-blue-950 sm:w-1/2'
+          class='order-2 mx-auto w-2/3 rounded-md border-2 border-blue-950 sm:order-1 sm:w-1/2'
           width={600}
           height={800}
         />
--- src/Header.tsx
+++ src/Header.tsx
@@ -3,19 +3,10 @@ import type { Component } from 'solid-js';
 const Header: Component = () => {
   return (
     <>
-      <header class='-mt-8 mb-4 flex flex-col justify-center rounded-sm bg-white pb-2 text-center'>
+      <header class='-mt-8 mb-4 flex flex-col justify-center rounded-sm bg-white text-center'>
         <div class='flex items-center justify-center sm:gap-1'>
           <object data='/logo.svg' type='image/svg+xml' class='max-w-sm md:max-w-md'></object>
-          {/* <h1 class='text-4xl font-bold uppercase text-white sm:text-5xl lg:text-6xl'>
-            ineedaroof.com
-          </h1> */}
         </div>
-        <p class='text-lg uppercase text-gray-400 lg:text-xl'>
-          By{' '}
-          <a href='https://youtu.be/dQw4w9WgXcQ' target='__blank' class='font-mukta underline'>
-            Baron Built Homes
-          </a>
-        </p>
       </header>
     </>
   );
