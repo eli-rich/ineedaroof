a48e8ff0839c1db8671d8bdec5461c392eecf029
Eli Richardson <elijahrich02@icloud.com>
2024-01-14 13:13:58 -0600

    fix links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,14 @@ const Footer: Component = () => {
     <>
       <footer class='mt-10 flex flex-col pb-5'>
         <a
-          href='https://youtu.be/dQw4w9WgXcQ'
+          href='https://baronbuilthomes.com'
           target='__blank'
           class='mb-2 text-center font-mukta text-xl text-gray-400 hover:underline'
         >
           Discover more of what Baron Built Homes has to offer!
         </a>
         <a
-          href='https://youtu.be/dQw4w9WgXcQ'
+          href='https://baronbuilthomes.com'
           target='__blank'
           class='mx-auto transform rounded-md bg-blue-500 p-2 text-xl font-bold uppercase text-white transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-blue-600'
         >
